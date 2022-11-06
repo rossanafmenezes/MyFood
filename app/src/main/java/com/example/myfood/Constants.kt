f821b0057349d8d2127af3631c406439d35d01e4
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-11-06 10:36:43 +0100

    Add Api class files

---
+++ app/src/main/java/com/example/myfood/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.myfood
+
+class Constants {
+    companion object {
+        const val API_KEY = "df2e679cf8ee4be7a1a631de83d2498b"
+        const val BASE_URL = "https://api.spoonacular.com/"
+    }
+}
